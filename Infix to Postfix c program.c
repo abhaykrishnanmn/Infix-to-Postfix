51cfe7d06e9d7ae488c0bcf6bc294b603c3e2226
Abhay Krishnan M N <75512915+abhaykrishnanmn@users.noreply.github.com>
2022-11-13 10:45:21 +0530

    Update Infix to Postfix c program.c

--- Infix to Postfix c program.c
+++ Infix to Postfix c program.c
@@ -1,92 +1,96 @@
 #include<stdio.h>
 #include<conio.h>
 #include<ctype.h>
- 
+
 #define MAX 50
- 
+
+char infix[30],postfix[30];
+
 typedef struct stack
 {
     int data[MAX];
     int top;
 }stack;
- 
-void infix_to_postfix(char infix[],char postfix[])
+
+char infix_to_postfix(char infix[])
 {
     stack s;
     char x,token;
     int i,j;    //i-index of infix,j-index of postfix
     init(&s);
     j=0;
- 
+
     for(i=0;infix[i]!='\0';i++)
     {
-        token=infix[i];
-        if(isalnum(token))
-            postfix[j++]=token;
-        else if(token=='(')
-               push(&s,'(');
-        else if(token==')')
-                while((x=pop(&s))!='(')
-                      postfix[j++]=x;
-        else
-             {
-                    while(precedence(token)<=precedence(top(&s))&&!empty(&s))
-                    {
-                        x=pop(&s);
-                        postfix[j++]=x;
-                    }
-                    push(&s,token);
-             }
+	token=infix[i];
+	if(isalnum(token))
+	    postfix[j++]=token;
+	else if(token=='(')
+	       push(&s,'(');
+	else if(token==')')
+		while((x=pop(&s))!='(')
+		      postfix[j++]=x;
+	else
+	     {
+		    while(precedence(token)<=precedence(top(&s))&&!empty(&s))
+		    {
+			x=pop(&s);
+			postfix[j++]=x;
+		    }
+		    push(&s,token);
+	     }
     }
- 
+
     while(!empty(&s))
     {
-        x=pop(&s);
-        postfix[j++]=x;
+	x=pop(&s);
+	postfix[j++]=x;
     }
- 
+
     postfix[j]='\0';
 }
- 
+
 int precedence(char x)
 {
     if(x=='(')
-        return(0);
+	return(0);
     if(x=='+'||x=='-')
-        return(1);
+	return(1);
     if(x=='*'||x=='/'||x=='%')
-        return(2);
- 
+	return(2);
+
     return(3);
 }
- 
-void init(stack *s)
+
+int init(stack *s)
 {
     s->top=-1;
+    return(0);
 }
- 
+
 int empty(stack *s)
 {
    if(s->top!=-1)
-        return(0);
- 
+	return(0);
+
    return(1);
 }
- 
+
 int full(stack *s)
 {
     if(s->top!=MAX-1)
-        return(0);
- 
+	return(0);
+
     return(1);
 }
- 
-void push(stack *s,int x)
+
+int push(stack *s,int x)
 {
     s->top=s->top+1;
     s->data[s->top]=x;
+    return(0);
 }
- 
+
 int pop(stack *s)
 {
     int x;
@@ -94,17 +98,18 @@ int pop(stack *s)
     s->top=s->top-1;
     return(x);
 }
- 
+
 int top(stack *p)
 {
     return (p->data[p->top]);
 }
- 
+
 void main()
 {
-    char infix[30],postfix[30];
-    printf("Enter an infix expression(eg: 5+2*4): ");
+    clrscr();
+    printf("\nEnter an infix expression(eg: 5+2*4): ");
     gets(infix);
-    infix_to_postfix(infix,postfix);
+    infix_to_postfix(infix);
     printf("\nPostfix expression: %s",postfix);
+    getch();
 }
